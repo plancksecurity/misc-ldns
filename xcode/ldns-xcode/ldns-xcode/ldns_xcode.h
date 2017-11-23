043eec62b5576606acc97a66ab69851b5f354073
Dirk Zimmermann <dirk@pep-project.org>
2017-11-23 11:31:41 +0100

    iOS: (empty) xcode project, .gitignore

---
+++ xcode/ldns-xcode/ldns-xcode/ldns_xcode.h
@@ -0,0 +1,19 @@
+//
+//  ldns_xcode.h
+//  ldns-xcode
+//
+//  Created by Dirk Zimmermann on 23.11.17.
+//  Copyright © 2017 pEp Security S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ldns_xcode.
+FOUNDATION_EXPORT double ldns_xcodeVersionNumber;
+
+//! Project version string for ldns_xcode.
+FOUNDATION_EXPORT const unsigned char ldns_xcodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ldns_xcode/PublicHeader.h>
+
+
