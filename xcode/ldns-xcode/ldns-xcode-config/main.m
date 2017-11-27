771b5a24b6282225e6b663598127d675ba21a6cc
Dirk Zimmermann <dirk@pep-project.org>
2017-11-27 13:20:56 +0100

    iOS: dummy target to run configure

---
+++ xcode/ldns-xcode/ldns-xcode-config/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ldns-xcode-config
+//
+//  Created by Dirk Zimmermann on 27.11.17.
+//  Copyright © 2017 pEp Security S.A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
