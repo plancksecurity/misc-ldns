00069d559f481fab3cd119d2e4785d84d16f85b2
Dirk Zimmermann <dirk@pep-project.org>
2017-12-04 15:00:31 +0100

    iOS: correct openssl path for configure

--- xcode/ldns-xcode/scripts/configure.sh
+++ xcode/ldns-xcode/scripts/configure.sh
@@ -8,7 +8,8 @@
 
 #
 # Sample invocation:
-# PROJECT_DIR=.. sh configure.sh
+# PROJECT_DIR=.. sh configure.sh # from the script directory
+# PROJECT_DIR=xcode/ldns-xcode/ sh xcode/ldns-xcode/scripts/configure.sh # from the ldns base
 #
 
 #
@@ -25,19 +26,19 @@
 #
 
 # macports must be in PATH
-PATH=$PATH:/opt/local/bin
+#PATH=$PATH:/opt/local/bin
 
 LDNS_BASE=$PROJECT_DIR/../..
 
-# Make sure the last part is correct
-SDK=$LDNS_BASE/../OpenSSL-for-iPhone/bin/iPhoneSimulator11.1-x86_64.sdk
+# Make sure the last part is correct, relative to $LDNS_BASE
+SDK=../OpenSSL-for-iPhone/bin/iPhoneSimulator11.1-x86_64.sdk
 
 pushd $LDNS_BASE
 git submodule update --init
 glibtoolize -ci
 autoreconf -fi
 autoreconf -fi
-./configure --disable-dane --disable-dane-verify --disable-dane-ta-usage --with-ssl=$SKD
+./configure --disable-dane --disable-dane-verify --disable-dane-ta-usage --with-ssl=$SDK
 popd
 mkdir -p $PROJECT_DIR/ldns
 mv $LDNS_BASE/ldns/config.h $PROJECT_DIR/ldns
